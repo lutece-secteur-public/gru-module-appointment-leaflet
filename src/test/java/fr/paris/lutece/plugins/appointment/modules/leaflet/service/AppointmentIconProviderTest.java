26fc020d61dd47e9ab4e70791dadc9606e978f7c
rdeniel <renaud.deniel@outlook.com>
2022-02-03 17:15:46 +0100

    APPLEAFLET-7 adding a unit test

---
+++ src/test/java/fr/paris/lutece/plugins/appointment/modules/leaflet/service/AppointmentIconProviderTest.java
@@ -0,0 +1,14 @@
+package fr.paris.lutece.plugins.appointment.modules.leaflet.service;
+
+import org.junit.Test;
+
+import fr.paris.lutece.test.LuteceTestCase;
+
+public class AppointmentIconProviderTest extends LuteceTestCase
+{
+    @Test
+    public void testAppointmentIconProvider(    )
+    {
+        assertTrue( true );
+    }
+}
